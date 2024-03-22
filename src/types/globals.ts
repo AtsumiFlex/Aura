d2ed0b4e21af8ab4fdea3cce1cb27e871857a364
AtsumiFlex <minecraftytom22@gmail.com>
2024-03-22 22:12:11 +0100

    Remove several code files
    The commit includes complete removal of several type, enum, and utility source files, including api.ts, base.ts, error.ts, formatting.ts, and more. The removal may indicate a significant restructuring or cleanup of project source code.

--- src/types/globals.ts
+++
@@ -1,3 +0,0 @@
-export type AuthorizationHeaderType = "Bearer" | "Bot";
-export type Snowflakes = string;
-export type GuildNavigationTypes = "browse" | "customize" | "guide";
--- src/utils/base.ts
+++
@@ -1,5 +0,0 @@
-import type { AuthorizationHeaderType } from "../types";
-
-export const BaseUrl = "https://discord.com/api";
-
-export const AuthorizationHeader = (type: AuthorizationHeaderType, token: string) => ({ Authorization: `${type} ${token}` });
