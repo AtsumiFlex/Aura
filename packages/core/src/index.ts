0a0c52325f1bedd566c603d37e5921a35591d1e3
AtsumiFlex <minecraftytom22@gmail.com>
2024-05-10 21:52:03 +0200

    Ajouts de quelque structures dans le packages "aurajs/core"
    Structure :
    - guilds

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -13,6 +13,7 @@ export * from "./libs/opcodes";
 export * from "./structures/applications";
 export * from "./structures/emojis";
 export * from "./structures/entitlements";
+export * from "./structures/guilds";
 export * from "./structures/moderations";
 export * from "./structures/polls";
 export * from "./structures/roles";
