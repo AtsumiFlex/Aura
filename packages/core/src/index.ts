73cd4e15c8019369a93cec2d9f6a0bdcbdc84dba
AtsumiFlex <minecraftytom22@gmail.com>
2024-05-10 19:20:07 +0200

    Ajouts de quelque structures dans le packages "aurajs/core" avec une amélioration des TSDoc
    Structure :
    - entitlements
    - roles
    - skus
    
    Libs :
    - oauth2

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,4 +6,10 @@ export * from "./globals/locales";
 export * from "./globals/markdowns";
 
 // ./libs/*
+export * from "./libs/oauth2";
 export * from "./libs/opcodes";
+
+// ./structures/*
+export * from "./structures/entitlements";
+export * from "./structures/roles";
+export * from "./structures/skus";
